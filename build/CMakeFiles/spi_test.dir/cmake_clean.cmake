94d9d79a235dcb6092f4153401612f1c065b8df8
wujansin <wujansin@gmail.com>
2023-01-06 19:24:24 +0800

    V0.01

---
+++ build/CMakeFiles/spi_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spi_test.dir/spi_test.c.o"
+  "spi_test"
+  "spi_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/spi_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
