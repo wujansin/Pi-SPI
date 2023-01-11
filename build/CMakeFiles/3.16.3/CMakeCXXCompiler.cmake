9632d768cfcb5e8d685ed76b2eebb4f3bdd15cf5
wujansin <wujansin@gmail.com>
2023-01-12 06:11:14 +0800

    20220112-1

--- build/CMakeFiles/3.16.3/CMakeCXXCompiler.cmake
+++ build/CMakeFiles/3.16.3/CMakeCXXCompiler.cmake
@@ -1,7 +1,7 @@
-set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_CXX_COMPILER "/bin/clang++-7")
 set(CMAKE_CXX_COMPILER_ARG1 "")
-set(CMAKE_CXX_COMPILER_ID "GNU")
-set(CMAKE_CXX_COMPILER_VERSION "10.2.1")
+set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_VERSION "7.0.1")
 set(CMAKE_CXX_COMPILER_VERSION_INTERNAL "")
 set(CMAKE_CXX_COMPILER_WRAPPER "")
 set(CMAKE_CXX_STANDARD_COMPUTED_DEFAULT "14")
@@ -14,19 +14,18 @@ set(CMAKE_CXX20_COMPILE_FEATURES "cxx_std_20")
 
 set(CMAKE_CXX_PLATFORM_ID "Linux")
 set(CMAKE_CXX_SIMULATE_ID "")
-set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT "")
+set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT "GNU")
 set(CMAKE_CXX_SIMULATE_VERSION "")
 
 
 
-
-set(CMAKE_AR "/usr/bin/arm-linux-gnueabihf-ar")
-set(CMAKE_CXX_COMPILER_AR "/usr/bin/arm-linux-gnueabihf-gcc-ar-10")
-set(CMAKE_RANLIB "/usr/bin/arm-linux-gnueabihf-ranlib")
-set(CMAKE_CXX_COMPILER_RANLIB "/usr/bin/arm-linux-gnueabihf-gcc-ranlib-10")
-set(CMAKE_LINKER "/usr/bin/arm-linux-gnueabihf-ld")
+set(CMAKE_AR "/bin/ar")
+set(CMAKE_CXX_COMPILER_AR "/usr/lib/llvm-7/bin/llvm-ar")
+set(CMAKE_RANLIB "/bin/ranlib")
+set(CMAKE_CXX_COMPILER_RANLIB "/usr/lib/llvm-7/bin/llvm-ranlib")
+set(CMAKE_LINKER "/bin/ld")
 set(CMAKE_MT "")
-set(CMAKE_COMPILER_IS_GNUCXX 1)
+set(CMAKE_COMPILER_IS_GNUCXX )
 set(CMAKE_CXX_COMPILER_LOADED 1)
 set(CMAKE_CXX_COMPILER_WORKS TRUE)
 set(CMAKE_CXX_ABI_COMPILED TRUE)
@@ -83,7 +82,7 @@ endif()
 
 
 
-set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "/usr/include/c++/10;/usr/include/arm-linux-gnueabihf/c++/10;/usr/include/c++/10/backward;/usr/lib/gcc/arm-linux-gnueabihf/10/include;/usr/local/include;/usr/include/arm-linux-gnueabihf;/usr/include")
+set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "/include/c++/8;/include/arm-linux-gnueabihf/c++/8;/include/c++/8/backward;/usr/include/clang/7.0.1/include;/usr/local/include;/usr/include/arm-linux-gnueabihf;/usr/include")
 set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "stdc++;m;gcc_s;gcc;c;gcc_s;gcc")
-set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "/usr/lib/gcc/arm-linux-gnueabihf/10;/usr/lib/arm-linux-gnueabihf;/usr/lib;/lib/arm-linux-gnueabihf")
+set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "/lib/gcc/arm-linux-gnueabihf/8;/lib/arm-linux-gnueabihf;/lib;/usr/lib/llvm-7/lib;/usr/lib/arm-linux-gnueabihf;/usr/lib")
 set(CMAKE_CXX_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- build/CMakeFiles/spi_test.dir/DependInfo.cmake
+++ build/CMakeFiles/spi_test.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/tbox/project/pi-io-test/spi/src/spi_test.cpp" "/home/tbox/project/pi-io-test/spi/build/CMakeFiles/spi_test.dir/spi_test.cpp.o"
+  "/home/pi/project/pi-io/Pi-SPI/src/spi_test.cpp" "/home/pi/project/pi-io/Pi-SPI/build/CMakeFiles/spi_test.dir/spi_test.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
